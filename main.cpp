6691e2519a33ea6679449693221fd38910edeb01
Cropi <xlakat01@stud.fit.vutbr.cz>
2019-05-17 22:39:36 +0200

    Usage specified

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <string.h>
 #include "buffer.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
     Buffer buffer;
     std::string type;
+
+    if(argc == 2 && strcmp(argv[1], "-h") == 0) {
+        std::cout << "Commands:\n" <<
+        "\tprint - print out all the elements of the buffer and erase it.\n"
+        "\tget - print out the first element of the buffer.\n"
+        "\texit - quit application.\n"
+        "\tnumber1:number2 - insert number1 into the buffer with priority of number2.\n";
+        return 0;
+    }
 
     while(42) {
         std::cin >> type;
